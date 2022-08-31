5ad1906786a2ff4818461de44ca0891979464ca3
Evans Ochieng <evansochieng121@gmail.com>
2022-08-31 12:01:09 +0300

    Initial fetch test

--- index.js
+++ index.js
@@ -2,19 +2,38 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     // Call search input
-    searchInput()
+    searchUniversity()
 })
 
+
+
+
+// Fetch data from API
+function fetchData(searchInput){
+    fetch(`http://universities.hipolabs.com/search?name=${searchInput}`)
+    .then( (response) => response.json())
+    .then( (result) => {
+        console.log(result)
+    })
+}
+
 // Grab user search input
-function searchInput(){
+function searchUniversity(){
     // Get the form element
     const form = document.getElementById("search-form")
 
-    return(
-        form.addEventListener('submit', (event) => {
-            event.preventDefault()
-            const search = event.target['search-input'].value;
-            event.target.reset();
-        })
-    )
+    // Grab user input and make a GET request
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const search = event.target['search-input'].value;
+        event.target.reset();
+
+        // Fetch university data
+        fetchData(search)
+        // fetch(`http://universities.hipolabs.com/search?name=${search}`)
+        // .then( (response) => response.json())
+        // .then( (result) => {
+        //     console.log(result)
+        // })
+    })
 }
